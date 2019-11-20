e9e9de16260e56226b8822d21b1a40a5fa318aaf
Corina Aionitoaie <c.aionitoaie@goldrepublic.com>
2019-11-20 17:32:47 +0100

    added empty methods

--- src/components/ShellGame/ShellGame.js
+++ src/components/ShellGame/ShellGame.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Shell from '../Shell/Shell';
 import "./style.css";
 
 function ShellGame(props) {
-  return (
-      <div class="shell-game">
-        <Shell hasPea={true}/>
-        <Shell />
-        <Shell />
-      </div>
-  );
+    const initialState = [ <Shell key="pea0" />,  <Shell key="pea1" hasPea={true}/>, <Shell key="pea2" />]
+    const [shells, setShells] = useState(initialState);
+
+    const resetGame = () => setShells(initialState);
+    const startGame = () => console.log("start game");
+    
+    return (
+        <div class="shell-game">
+            {shells}
+        </div>
+    );
 }
 
 export default ShellGame;
